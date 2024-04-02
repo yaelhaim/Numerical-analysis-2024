b216b3ba758248dec1a02cd1f273491aaa4551a0
yaelhaim <129064540+yaelhaim@users.noreply.github.com>
2024-04-02 21:40:42 +0300

    Add files via upload

--- newtonRaphson.py
+++ newtonRaphson.py
@@ -1,14 +1,40 @@
+"""
+Input function f, initial guess x0, tolerance TOL, maximum iterations N
+
+For n from 1 to N:
+    Calculate the derivative of the function at x0:
+        df = derivative of f at x0
+
+    If df ≈ 0:
+        Print "Derivative is approximately zero. Algorithm failed."
+        Exit
+
+    Calculate the change in x using Newton-Raphson method:
+        d = f(x0) / df
+
+    If the absolute value of d is less than TOL:
+        Return x0 as the approximate root
+
+    Update x0 for the next iteration:
+        x0 = x0 - d
+
+Return x0 as the current approximation if the maximum iterations are reached
+
+"""
 from colors import bcolors
 
+def derivative(f, x, h=1e-6):
+    return (f(x + h) - f(x)) / h
 
-def newton_raphson(f, df, p0, TOL, N=50):
-    print("{:<10} {:<15} {:<15} ".format("Iteration", "po", "p1"))
+def newton_raphson(f, p0, TOL, N=50):
+    print("{:<10} {:<15} {:<15} ".format("Iteration", "p0", "p1"))
     for i in range(N):
-        if df(p0) == 0:
-            print( "Derivative is zero at p0, method cannot continue.")
+        df_p0 = derivative(f, p0)
+        if df_p0 == 0:
+            print("Derivative is zero at p0, method cannot continue.")
             return
 
-        p = p0 - f(p0) / df(p0)
+        p = p0 - f(p0) / df_p0
 
         if abs(p - p0) < TOL:
             return p  # Procedure completed successfully
@@ -17,11 +43,11 @@ def newton_raphson(f, df, p0, TOL, N=50):
     return p
 
 
+
 if __name__ == '__main__':
     f = lambda x: x**3 - 3*x**2
-    df = lambda x: 3*x**2 - 6*x
     p0 = -5
     TOL = 1e-6
     N = 100
-    roots = newton_raphson(f, df,p0,TOL,N)
+    roots = newton_raphson(f,p0,TOL,N)
     print(bcolors.OKBLUE,"\nThe equation f(x) has an approximate root at x = {:<15.9f} ".format(roots),bcolors.ENDC,)
--- secant_method.py
+++ secant_method.py
@@ -1,3 +1,24 @@
+"""
+Input function f, initial guesses x0 and x1, tolerance TOL, maximum iterations N
+
+For n from 1 to N:
+    Calculate the slope using secant method:
+        slope = (f(x1) - f(x0)) / (x1 - x0)
+
+    Calculate the change in x using the secant method:
+        d = f(x1) / slope
+
+    If the absolute value of d is less than TOL:
+        Return x1 as the approximate root
+
+    Update x0 and x1 for the next iteration:
+        x0 = x1
+        x1 = x1 - d
+
+Return x1 as the current approximation if the maximum iterations are reached
+
+"""
+
 from colors import bcolors
 
 
